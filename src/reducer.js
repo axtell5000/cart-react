46b6884f7c2ad57ef0194d4c0d19a11239c2b8a8
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-23 11:49:11 +0200

    remove single item, includes reducer and actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,14 @@
 const reducer = (state, action) => {
   switch (action.type) {
     case 'CLEAR_CART':
-      return { ...state, cart: [] };  
+      return { ...state, cart: [] };
+      
+    case 'REMOVE':
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload)
+      }
+      
     default:
       return state;
   }
