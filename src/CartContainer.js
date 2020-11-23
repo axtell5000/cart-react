26309212b6c8a2136741f208aee6bab799e3fd63
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-23 11:24:01 +0200

    added new styles, basic structure as well as startinng context and reducer

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from 'react';
+
+import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart, total } = useGlobalContext();
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -13,8 +14,9 @@ const CartContainer = () => {
           <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
-    )
+    );
   }
+
   return (
     <section className='cart'>
       {/* cart header */}
@@ -32,18 +34,19 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            total <span>${total}</span>
           </h4>
         </div>
         <button
           className='btn clear-btn'
           onClick={() => console.log('clear cart')}
+          title='Clear cart'
         >
           clear cart
         </button>
       </footer>
     </section>
-  )
+  );
 }
 
-export default CartContainer
+export default CartContainer;
--- src/CartItem.js
+++ src/CartItem.js
@@ -11,13 +11,14 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <button
           className='remove-btn'
           onClick={() => console.log('remove item')}
+          title="Remove item"
         >
           remove
         </button>
       </div>
       <div>
         {/* increase amount */}
-        <button className='amount-btn' onClick={() => console.log('increase')}>
+        <button className='amount-btn' onClick={() => console.log('increase')} title="Increase amount">
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
@@ -25,7 +26,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
         {/* amount */}
         <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className='amount-btn' onClick={() => console.log('decrease')}>
+        <button className='amount-btn' onClick={() => console.log('decrease')} title="Decrease amount">
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
