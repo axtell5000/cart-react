dfcf973ae23766a1e272e4ed8fa6604f9ac8bf2e
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-23 12:15:36 +0200

    increase and decrease the item amounts in the cart

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,30 @@ const reducer = (state, action) => {
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload)
-      }
+      };
+
+    case 'INCREASE':
+      // create a temp value to protect the state property
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount + 1 }
+        }
+        return cartItem
+      });
+      return { ...state, cart: tempCart };
+
+    case 'DECREASE':
+      // create a temp value to protect the state property, also if amount is zero - remove item from the list
+      let temporaryCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+
+      return { ...state, cart: temporaryCart }
       
     default:
       return state;
